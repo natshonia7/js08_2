cb648d238731ab002188a1818dadd7d32415f620
natia <nati.shonia37@gmail.com>
2022-05-25 17:33:48 +0400

    Create module.js

---
+++ module.js
@@ -0,0 +1,7 @@
+// export default function postDate(url = '/api/register') {
+//     axios.post(url).then(item => {
+//         console.log(item)
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// }
